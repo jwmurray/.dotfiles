49fab790c944bf54284b7dacda97f27ecd901e3e
jmurray <john@gardenway.org>
2021-02-27 06:02:27 -0700

    Add homebew to fish path.

--- fish.symlink/config.fish
+++ fish.symlink/config.fish
@@ -1,6 +1,7 @@
 set -gx PATH $HOME/.cargo/bin $PATH
 set -gx PATH $HOME/.dotfiles/bin $PATH
 set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /usr/local/pycharm/bin $PATH
 set -gx PATH /usr/local/clion/bin $PATH
 set -gx PATH /usr/local/node/bin $PATH
