0ad172e6cdf4c1a54914b7185b665da7cad58557
jmurray <john@gardenway.org>
2021-03-02 11:35:19 -0700

    update bash login for mac

--- fish.symlink/config.fish
+++ fish.symlink/config.fish
@@ -5,9 +5,9 @@ set -gx PATH /usr/local/bin $PATH
 set -gx PATH /usr/local/pycharm/bin $PATH
 set -gx PATH /usr/local/clion/bin $PATH
 set -gx PATH /usr/local/node/bin $PATH
-# set -gx PATH /Library/Frameworks/Python.framework/Versions/Current/bin
-
 set -gx PATH /opt/homebrew/bin $PATH
+
+
 
 #setxkbmap -option ctrl:nocaps
 
