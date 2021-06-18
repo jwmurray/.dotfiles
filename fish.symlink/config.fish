716dc19bf31882b63bd551366f7bdafe0a8647a7
jmurray <john@gardenway.org>
2021-06-18 05:11:43 -0600

    Next commit

--- fish.symlink/config.fish
+++ fish.symlink/config.fish
@@ -8,7 +8,9 @@ set -gx PATH /usr/local/node/bin $PATH
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /opt/local/bin $PATH
 set -gx PATH /Users/jmurray/code/vasolabs/controller $PATH
+set -gx PATH $PYENV_ROOT/bin $PATH
 
+set -gx PYENV_ROOT $HOME/.pyenv
 
 #setxkbmap -option ctrl:nocaps
 
@@ -22,3 +24,7 @@ set -gx PATH /Users/jmurray/code/vasolabs/controller $PATH
 # set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
 #
 # pyenv init - | source
+
+# Setting PATH for Python 3.9
+# The original version is saved in /Users/jmurray/.config/fish/config.fish.pysave
+# set -x PATH "/Library/Frameworks/Python.framework/Versions/4.9/bin" "$PATH"
